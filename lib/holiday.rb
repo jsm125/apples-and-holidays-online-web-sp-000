b90f80f094c9da082a8b497fe90695c0eb337200
Matt Im <matthew.seiim@gmail.com>
2020-05-04 19:48:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_supplies[:spring][:presidents_day] = masks
-  end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
