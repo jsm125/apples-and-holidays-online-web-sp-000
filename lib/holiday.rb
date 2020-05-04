4512f088461fd7b0fd3f0d3a100bc222e95e2cab
Matt Im <matthew.seiim@gmail.com>
2020-05-04 20:00:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.split.capitalize.join
-  
+  split_array = holiday_hash.split
+  split_array.capitalize.join
+  split_array
 end
 
 def all_holidays_with_bbq(holiday_hash)
