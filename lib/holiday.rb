cf64e1d3ed489e3202b3b80c467df1b791c2709d
Matt Im <matthew.seiim@gmail.com>
2020-05-04 20:57:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,6 @@ def all_holidays_with_bbq(holiday_hash)
       holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
+all_holidays_with_bbq(holiday_hash)
 end
 
-all_holidays_with_bbq(holiday_hash)
